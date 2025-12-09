12162d4cd55077ba5197cca656fcb5802e50e09b
Dewan Mehedi Hasan <tanjildewan10@gmail.com>
2025-12-09 09:03:19 +0600

    implement order orderItem and payment system

--- shop/views.py
+++ shop/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render,redirect,get_object_or_404
 from django.contrib.auth import authenticate,login,logout
 from django.contrib import messages
-from .forms import RegistrationForm,RatingForm
+from .forms import RegistrationForm,RatingForm,CheckoutForm
 from . import models
 from django.db.models import Max,Min,Avg,Q
 # filter on price,category,rating
@@ -149,4 +149,129 @@ def rate_product(request,product_id):
     }    
 
     return render(request,'',context)    
+
+
+def cart_add(request,product_id):
+    product = get_object_or_404(models.Product,id=product_id)
+
+    try:
+        cart = models.Cart.objects.get(user=request.user)
+    except models.Cart.DoesNotExist:
+        cart = models.Cart.objects.create(user=request.user)    
+
+    try:
+        cart_item = models.CartItem.objects.get(cart=cart,product=product)
+        cart_item.quantity += 1
+        cart_item.save()
+    except models.CartItem.DoesNotExist:
+        models.CartItem.objects.create(cart=cart,product=product,quantity = 1)    
+
+    messages.success(request,f'{product.name} has been added successfully')
+    return redirect('')       
+
+
+def cart_update(request,product_id):
+    cart = get_object_or_404(models.Cart,user=request.user)
+    product = get_object_or_404(models.Product,id=product_id)
+    cart_item = get_object_or_404(models.CartItem,cart=cart,product=product)
+
+    quantity = int(request.POST.get('quantity',1))
+
+    if quantity <= 0:
+        cart_item.delete()
+        messages.success(request,f'{product.name} has been deleted from your cart')    
+    else:
+        cart_item.quantity = quantity
+        cart_item.save()
+        messages.success(request,'Cart updated successfully')
+    return redirect('')     
+
+
+def cart_delete(request,product_id):
+    cart = get_object_or_404(models.Cart,user=request.user)
+    product = get_object_or_404(models.Product,id=product_id)
+    cart_item = get_object_or_404(models.CartItem,cart=cart,product=product)
+    cart_item.delete()
+    messages.success(request,f'{product.name} has been deleted successfully')
+    return redirect('')
+
+
+def cart_details(request):
+    try:
+        cart = models.Cart.objects.get(user=request.user)
+    except models.Cart.DoesNotExist:
+        cart = models.Cart.objects.create(user=request.user)    
+    return render(request,'',{'cart':cart})    
+
+
+def checkout(request):
+    try:
+       cart = models.Cart.objects.get(user=request.user)
+       if not cart.cart_items.exists():
+           messages.warning(request,'Your cart is empty')
+           return redirect('')
+    except models.Cart.DoesNotExist:   
+        messages.warning(request,"Your cart doesn't exists")
+        return redirect('')
+    
+    if request.method == 'POST':
+        form = CheckoutForm(request.POST)
+        if form.is_valid():
+          order = form.save(commit=False)
+          order.user = request.user
+          order.save()
+
+        for item in cart.cart_items.all():
+           models.OrderItem.objects.create(
+           order = order,
+           product = item.product,
+           price = item.product.price,
+           quantity = item.quantity
+         )
+    
+        cart.cart_items.all().delete()    
+        request.session['order_id'] = order.id  
+        return redirect('')
+   
+    else:
+        form = CheckoutForm()
+    context = {
+        'cart':cart,
+        'form':form
+    }    
+    return render(request,'',context)     
+
+
+def payment_success(request,order_id):
+    order = get_object_or_404(models.Order,id=order_id,user=request.user)     
+    order.paid = True  
+    order.status = 'processing'
+    order.tns_id = order.id
+    order.save()
+    order_item = order.order_items.all()
+
+    for item in order_item:
+        product = item.product
+        product.stock -= item.quantity
+
+    if product.stock < 0:
+        product.stock = 0
+    product.save()    
+
+    messages.success(request,'Payment successfull')
+
+    return render(request,'',{'order':order})
+
+
+def payment_fail(request,order_id):
+    order = get_object_or_404(models.Order,id=order_id,user=request.user)
+    order.status = 'canceled'
+    order.save()
+    return redirect('')
+
+def payment_calcel(request,order_id):
+    order = get_object_or_404(models.Order,id=order_id,user=request.user)
+    order.status = 'canceled'
+    order.save()
+    return redirect('')
 
